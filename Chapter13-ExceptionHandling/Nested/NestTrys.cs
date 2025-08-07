947305ca021551b15bbb8b49e4728e1702fc6ed2
Giorgi Dzagania <giorgidzagania2299@gmail.com>
2025-08-07 22:19:15 +0300

    Nested try

---
+++ Chapter13-ExceptionHandling/Nested/NestTrys.cs
@@ -0,0 +1,29 @@
+namespace Chapter13_ExceptionHandling.Nested;
+
+// Use a nested try block.
+
+public class NestTrys
+{
+    public static void nest() {
+        
+        // Here, numer is longer than denom.
+        int[] numer = { 4, 8, 16, 32, 64, 128, 256, 512 };
+        int[] denom = { 2, 0, 4, 4, 0, 8 };
+        try { // outer try
+            for(int i = 0; i < numer.Length; i++) {
+                try { // nested try
+                    Console.WriteLine(numer[i] + " / " +
+                                      denom[i] + " is " +
+                                      numer[i]/denom[i]);
+                }
+                catch (DivideByZeroException) {
+                    Console.WriteLine("Can't divide by Zero!");
+                }
+            }
+        }
+        catch (IndexOutOfRangeException) {
+            Console.WriteLine("No matching element found.");
+            Console.WriteLine("Fatal error -- program terminated.");
+        }
+    }
+}
--- Chapter13-ExceptionHandling/Program.cs
+++ Chapter13-ExceptionHandling/Program.cs
@@ -1,16 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using Chapter13_ExceptionHandling.Exc;
+using Chapter13_ExceptionHandling.Nested;
 
 class Program
 {
     static void Main()
     {
         testExc();
+        testNested();
+    }
+
+    static void testNested()
+    {
+        Console.WriteLine("-- Nested --");
+        NestTrys.nest();
+        Console.WriteLine("");
     }
 
     static void testExc()
     {
+        Console.WriteLine("-- EXC --");
         ExcDemo.test();
+        Console.WriteLine("");
     }
 }
